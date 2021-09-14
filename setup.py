37706762ff69d5d4d26a5f11eed18dca8a9c8d1f
HetDaftary <hetdaftary@gmail.com>
2021-09-14 16:23:58 +0530

    Updating documentation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['SimplePasswordManagerBackend'], # this must be the same as the name above
   version = '0.5',
   description = 'Simple Password Manager Backend Module',
-  long_description=README.md,
+  long_description="./README.md",
   long_description_content_type='text/markdown',
   author = 'HetDaftary',
   author_email = 'codingjungle1729@gmail.com',
