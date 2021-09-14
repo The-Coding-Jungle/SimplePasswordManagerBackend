bfe6ac56a16ae0eab0256a30e5915d883c42a5c1
HetDaftary <hetdaftary@gmail.com>
2021-09-14 16:10:59 +0530

    Updating documentation

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'SimplePasswordManagerBackend',
   packages = ['SimplePasswordManagerBackend'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.5',
   description = 'Simple Password Manager Backend Module',
+  long_description=README.md,
+  long_description_content_type='text/markdown',
   author = 'HetDaftary',
   author_email = 'codingjungle1729@gmail.com',
   url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend',
-  download_url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend/releases/tag/0.3',
   keywords = ['add', 'sub', 'tests', 'password', 'password-manager'], 
   classifiers = [],
   dependencies = ['pyperclip', 'pybase64', 'pycryptodome', 'pysqlite3', 'clipboard'],
