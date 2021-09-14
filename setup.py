988b74128b43ebdd02a362991b39389a213d4079
TheCodingJungle <codingjungle1729@gmail.com>
2021-09-14 14:23:35 +0530

    Adding code

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   author = 'Het Daftary',                   # Type in your name
   author_email = 'hetdaftary@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend/archive/refs/tags/1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend/archive/refs/tags/1.2.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'clipboard',
