6414eeb6b6e26b546cb669d9c03da5aaa38e1e70
HetDaftary <hetdaftary@gmail.com>
2021-09-16 16:13:05 +0530

    Updating

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 setup(
   name = 'SimplePasswordManagerBackend',         # How you named your package folder (MyLib)
   packages = ['SimplePasswordManagerBackend'],   # Chose the same as "name"
-  version = '1.0.0',      # Start with a small number and increase it with every change you make
+  version = '1.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Simple password manager backend',   # Give a short description about your library
+  long_description=open('README.md').read(),
   author = 'Het Daftary',                   # Type in your name
   author_email = 'hetdaftary@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend/archive/refs/tags/1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/The-Coding-Jungle/SimplePasswordManagerBackend/archive/refs/tags/1.5.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'clipboard',
